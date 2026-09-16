1652689e2cf781b36cc42192f20de44729783402
MohammadRokib <rokibk548@gmail.com>
2026-09-16 15:56:05 +0600

    feat(routing): add about page and route
    - add AboutPage standalone component under pages/
    - register 'about' path -> AboutPage in app.routes
    
    Signed-off-by: MohammadRokib <rokibk548@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page';
+import { AboutPage } from './pages/about-page';
 
-export const routes: Routes = [{ path: '', component: HomePage }];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'about', component: AboutPage },
+];
---
+++ src/app/pages/about-page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.html',
+})
+export class AboutPage {}
